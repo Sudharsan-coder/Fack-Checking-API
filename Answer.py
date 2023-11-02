bd00bf94681f565919307f3a82a6c84e17cb3966
sudharsan-coder <sudharsan220103@gmail.com>
2023-11-02 20:08:55 +0530

    added pip lib directly in py

--- Answer.py
+++ Answer.py
@@ -8,7 +8,11 @@ try:
   import json
   from transformers import logging
   logging.set_verbosity_error()
-
+  import pip
+  pip.main(["install", "pandas"])
+  pip.main(["install", "sentence-transformers"])
+  pip.main(["install", "torch"])
+  pip.main(["install", "transformers"])
 
   data = read_csv("datas.csv")
   links = data['Links'].tolist()
